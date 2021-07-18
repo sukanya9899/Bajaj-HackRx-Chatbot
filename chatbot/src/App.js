8e7de1886e3870feb5fc7147417d656177210fcc
Sukanya Bhattacharya <sukanyabhattacharya99@gmail.com>
2021-07-18 13:03:14 +0530

    init

---
+++ chatbot/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import ChatBotRobot from './Chatbot.compoenent';
+
+function App() {
+  return (
+    <span>
+      <ChatBotRobot />
+      <div className="App">
+        <header className="App-header">
+        <p>
+            HackRx 2.0 Test Website
+        </p>
+        </header>
+      </div>
+    </span>
+  );
+}
+
+export default App;
---
+++ chatbot/src/Chatbot.compoenent.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Launcher } from 'react-chat-window'
+import io from 'socket.io-client';
+import botlogo from './BOTIMG.png'
+
+class ChatBotRobot extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            messageList: [],
+            socket: io("http://localhost:3000"),
+            room: "user1",
+        }
+
+    }
+
+    UNSAFE_componentWillMount() {
+        this._sendMessage("Hey there !");
+    }
+
+    componentDidMount() {
+        this.state.socket.connect(true);
+        this.state.socket.emit('join', this.state.room);
+
+        this.state.socket.on("send-msg-response", async (msg) => {
+            this.state.messageList.pop();
+            await this.setState({
+                messageList: [...this.state.messageList]
+            })
+
+            this._sendMessage(msg);
+        })
+
+    }
+
+    async _onMessageWasSent(message) {
+        await this.setState({
+            messageList: [...this.state.messageList, message]
+        })
+
+        this._sendMessage("••••");
+        await this.state.socket.emit('new-msg', { msg: message.data.text, room: this.state.room })
+
+    }
+
+    _sendMessage(text) {
+        if (text.length > 0) {
+            this.setState({
+                messageList: [...this.state.messageList, {
+                    author: 'them',
+                    type: 'text',
+                    data: { text }
+                },]
+            })
+        }
+    }
+
+    render() {
+
+        return (
+            <div id="chatbox" className="chatbox">
+                <Launcher 
+                    agentProfile={{
+                        teamName: 'Chatbot',
+                        imageUrl: botlogo,
+                    }}
+                    onMessageWasSent={this._onMessageWasSent.bind(this)}
+                    messageList={this.state.messageList}
+                    showEmoji
+                />
+            </div>
+        );
+    }
+}
+
+export default ChatBotRobot;
